b6cc9ed177aff840eb4ec93465fac463eed69fd7
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-07-04 19:15:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
 //your code here
+class OutOfRangeError extends Error {
+	constructor(arg) {
+		super(`Expression should only consist of integers and +-/* characters and not ${arg}`);
+		this.name = "OutOfRangeError";
+	}
+}
+
+class InvalidExprError extends Error {
+	constructor() {
+		super("Expression should not have an invalid combination of expression");
+		this.name = "InvalidExprError";
+	}
+}
+
+function evalString(expression) {
+	try {
+		expression = expression.trim();
+
+		// Starts with invalid operator
+		if(/^[+/*]/.test(expression)) {
+			throw new SyntaxError(
+				"Expression should not start with invalid operator"
+			);
+		}
+
+		// Ends with invalid operator
+		if(/[+\-/*]$/.test(expression)) {
+			throw new SyntaxError(
+				"Expression should not end with invalid operator"
+			);
+		}
+
+		// Invalid characters
+		let invalid = expression.match(/[^0-9+\-/*\s]/);
+		if(invalid) {
+			throw new OutOfRangeError(invalid[0]);
+		}
+
+		// Invalid operator combinations
+		if(/[+\-/*]{2,}/.test(expression.replace(/\s+/g, ""))) {
+			throw new InvalidExprError();
+		}
+
+		return eval(expression);
+		
+	} catch (err) {
+		throw err;
+	}
+}
